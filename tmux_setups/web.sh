d061538c95898127641e2106cdccb7ba261965dd
Fernando Gasperi Jabalera <fjabalera@manas.com.ar>
2016-02-27 11:37:34 -0300

    Removed the uses of autojump from web tmux setup script

--- tmux_setups/web.sh
+++ tmux_setups/web.sh
@@ -4,11 +4,11 @@ if [ $# -gt 0 ]; then
 fi
 # vim window
 tmux new-window -n web-vim -t manas
-tmux send-keys -t manas:web-vim 'j web' C-m
+tmux send-keys -t manas:web-vim 'cd ~/cepheid/cepheid-web' C-m
 tmux send-keys -t manas:web-vim "git checkout $BRANCH_NAME" C-m
 tmux send-keys -t manas:web-vim 'vim' C-m
 # web server
 tmux new-window -n web-server -t manas
-tmux send-keys -t manas:web-server 'j web' C-m
+tmux send-keys -t manas:web-server 'cd ~/cepheid/cepheid-web' C-m
 tmux send-keys -t manas:web-server "git checkout $BRANCH_NAME" C-m
 tmux send-keys -t manas:web-server '../start_web_server.sh' C-m
