5440e51eaefe7d8e36ad4ee5184ab5cc81dcc1f9
Fernando Gasperi Jabalera <fgasperijabalera@gmail.com>
2020-07-11 11:45:50 -0300

    Sigmind zshrc

---
+++ zsh/sigmind.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# I believe this was to execute somthing related to Python, venv?
+PATH="${PATH}:$HOME/.local/bin"
+
+# To install cordova globally only for the user and avoid using /usr/lib
+PATH="${PATH}:$HOME/.node_modules/bin/bin"
+export npm_config_prefix=~/.node_modules/bin
+
+# AWS
+alias aws_sigmind_test='ssh -i "~/.ssh/ar-lab-test-apirest.pem" ubuntu@ec2-54-166-192-65.compute-1.amazonaws.com'
+
+
+
