04ec8908c8a927ebcd3a17ae0de9c5259b892ed3
Fernando Gasperi Jabalera <fgasperi@fgasperi-mbp.dhcp.thefacebook.com>
2021-05-23 19:11:58 +0100

    Add DOTFILES variable

--- bootstrap/setup_darwin.sh
+++ bootstrap/setup_darwin.sh
@@ -1,5 +1,7 @@
 # Preconditions
 cd $HOME
+# Path to the dotfiles repo, example: /User/fgasperi/repos/dotfiles
+DOTFILES=$1
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -12,7 +14,7 @@ sudo scutil --set HostName apel
 # look for `which zsh` on the /etc/shells because if it isn't there it will fail
 chsh -s $(which zsh)
 zsh
-ln -s dotfiles/zshrc .zshrc
+ln -s $DOTFILES/zshrc .zshrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'source $HOME/.zshrc.pre-oh-my-zsh' >> $HOME/.zshrc
@@ -24,11 +26,11 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 ln -s dotfiles/tmux/tmux.conf .tmux.conf
 
 # vim
-ln -s dotfiles/vim/vimrc .vimrc
+ln -s $DOTFILES/vim/vimrc .vimrc
 mkdir .vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd .vim
-ln -s ../dotfiles/vim/plugins.vim plugins.vim
+ln -s $DOTFILES/vim/plugins.vim plugins.vim
 cd -
 # this rm could fail, doesn't matter, it's just to perform the link safely
 rm -r .vim/ftplugin
@@ -37,11 +39,11 @@ ln -s dotfiles/vim/ftplugin .vim/ftplugin
 vim +PluginInstall +qall
 
 # setup emacs
-ln -s dotfiles/emacs/init.el ~/.emacs.d/init.el
+ln -s $DOTFILES/emacs/init.el ~/.emacs.d/init.el
 
 # git
-ln -s dotfiles/git/gitconfig .gitconfig
-ln -s dotfiles/git/gitignore_global .gitignore_global
+ln -s $DOTFILES/git/gitconfig .gitconfig
+ln -s $DOTFILES/git/gitignore_global .gitignore_global
 
 cd $HOME
 
