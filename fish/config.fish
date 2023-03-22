3b8724b73738d42b01410d06872eccf003496c8e
Fernando Gasperi Jabalera <fgasperi@fb.com>
2023-03-22 19:34:46 +0000

    Add Fish settings

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+export PATH="$HOME/homebrew/bin:$HOME/homebrew/sbin:$PATH"
+export PATH="$HOME/.local/dev_bins:$PATH"
+
+## shortcut
+alias reload!='source ~/.config/fish/config.fish'
+alias config='nvim ~/.config/fish/config.fish'
+alias l='ls -lah'
+
+
