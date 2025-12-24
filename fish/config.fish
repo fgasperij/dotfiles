b74cf14f11e5b3bc8bf3e1eae9c8d4df57689398
Fernando Gasperi <fgasperijabalera@gmail.com>
2025-12-24 08:21:49 -0300

    Remove some FB internal settings for VS Code

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,20 @@
-export PATH="$HOME/homebrew/bin:$HOME/homebrew/sbin:$PATH"
-export PATH="$HOME/.local/dev_bins:$PATH"
+if status is-interactive
+    
+    fish_add_path -a /opt/homebrew/bin
+    fish_add_path -a /Users/ferg/Downloads/chromedriver-mac-arm64/chromedriver
+    fish_add_path -a /Users/ferg/Library/Python/3.9/bin
+    
+    ## shortcut
+    alias reload!='source ~/.config/fish/config.fish'
+    alias config='code ~/.config/fish/config.fish'
+    alias l='ls -lah'
 
-## shortcut
-alias reload!='source ~/.config/fish/config.fish'
-alias config='nvim ~/.config/fish/config.fish'
-alias l='ls -lah'
+    # vim mode
+    set -g fish_key_bindings fish_vi_key_bindings
+    set fish_cursor_default block  # Normal mode
+    set fish_cursor_insert line    # Insert mode
+    set fish_cursor_visual block   # Visual mode
+    bind --mode insert --sets-mode default jk repaint
 
-
+end
+export PATH="$HOME/.local/bin:$PATH"
---
+++ osx/remap_keys.sh
@@ -0,0 +1,2 @@
+# remap ±/§ to `/~
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
